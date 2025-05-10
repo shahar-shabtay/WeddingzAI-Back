8bbf3b096f7d8f5f032d5dbe5da715cfdfe1cc83
zivklein21 <zivklein21@gmail.com>
2025-05-10 22:57:23 +0300

    Fix the scrape on djs

--- src/routes/vendor_routes.ts
+++ src/routes/vendor_routes.ts
@@ -1,7 +1,17 @@
-import { Router } from "express";
-import { scrapeOneDjController } from "../controllers/dj-controller";
+import { Router } from 'express';
+import authMiddleware from '../common/auth-middleware';
+import vendorsCtrl from '../controllers/vendor-controller';
 
 const router = Router();
-router.post("/scrape-one", scrapeOneDjController);
+
+// CRUD via BaseController:
+router.get(   '/djs',        authMiddleware, vendorsCtrl.getAll.bind(vendorsCtrl)      );
+router.get(   '/djs/mine',   authMiddleware, vendorsCtrl.getMine.bind(vendorsCtrl)       );
+router.get(   '/djs/:id',    authMiddleware, vendorsCtrl.getById.bind(vendorsCtrl)       );
+router.delete('/djs/:id',    authMiddleware, vendorsCtrl.deleteItem.bind(vendorsCtrl)   );
+
+// Scraping endpoints:
+router.post(  '/djs/find',   authMiddleware, vendorsCtrl.find        );
+router.post(  '/djs/scrape', authMiddleware, vendorsCtrl.scrape      );
 
 export default router;
