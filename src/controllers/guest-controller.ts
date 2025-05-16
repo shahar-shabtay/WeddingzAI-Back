2f68f06b39a03cdb3fab8353ca1430fcec646c4c
Gal Ternovsky <gal.ternovsky@walkme.com>
2025-05-16 14:08:14 +0300

    added gmail tokens

--- src/controllers/guest-controller.ts
+++ src/controllers/guest-controller.ts
@@ -2,7 +2,7 @@ import { Response, NextFunction } from 'express';
 import guestModel, { IGuest } from '../models/guest-model';
 import { BaseController } from './base-controller';
 import { AuthRequest } from '../common/auth-middleware';
-import { sendInvitationEmails } from '../services/gmail-service.ts';
+import { sendInvitationEmails } from '../services/gmail-service';
 
 class GuestsController extends BaseController<IGuest> {
   constructor() {
