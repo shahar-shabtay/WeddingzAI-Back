227ff50c7807d7e83c73b24034d59f875d9791f5
Gal Ternovsky <gal.ternovsky@walkme.com>
2025-05-16 14:10:55 +0300

    added guest routes

--- src/routes/guest-routes.ts
+++ src/routes/guest-routes.ts
@@ -7,12 +7,15 @@ import guestsController from '../controllers/guest-controller';
 const router = Router();
 
 // CRUD via BaseController
-router.get(   '/guests',        authMiddleware, guestsController.getAll.bind(guestsController));
-router.get(   '/guests/mine',   authMiddleware, guestsController.getMine.bind(guestsController));
-router.get(   '/guests/:id',    authMiddleware, guestsController.getById.bind(guestsController));
-router.delete('/guests/:id',    authMiddleware, guestsController.deleteItem.bind(guestsController));
+router.get(    '/guests',             authMiddleware, guestsController.getAll.bind(guestsController));
+router.get(    '/guests/mine',        authMiddleware, guestsController.getMine.bind(guestsController));
+router.get(    '/guests/:id',         authMiddleware, guestsController.getById.bind(guestsController));
 
-// POST /guests
-router.post(  '/guests',        authMiddleware, guestsController.create);
+router.post(   '/guests',             authMiddleware, guestsController.create);
+router.post(   '/guests/send-invitation', authMiddleware, guestsController.sendInvitation);
+
+router.delete( '/guests/:id',         authMiddleware, guestsController.remove); 
+
+router.put(    '/guests/:id',         authMiddleware, guestsController.update);
 
 export default router;
